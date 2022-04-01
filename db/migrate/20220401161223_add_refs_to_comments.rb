d225f06dab4e61ce391d4a77b2b9883ec4611b13
Oliver Coimbra <olivercoimbra@gmail.com>
2022-04-01 12:58:04 -0400

    Add index

--- db/migrate/20220401161223_add_refs_to_comments.rb
+++ db/migrate/20220401161223_add_refs_to_comments.rb
@@ -3,5 +3,7 @@ class AddRefsToComments < ActiveRecord::Migration[7.0]
     add_reference :comments, :user, null: false, foreign_key: true
     add_reference :comments, :post, null: false, foreign_key: true
     rename_column :comments, :user_id, :author_id
+    add_index :comments, :user
+    add_index :comments, :post
   end
 end
--- db/migrate/20220401161642_add_refs_to_likes.rb
+++ db/migrate/20220401161642_add_refs_to_likes.rb
@@ -3,5 +3,7 @@ class AddRefsToLikes < ActiveRecord::Migration[7.0]
     add_reference :likes, :user, null: false, foreign_key: true
     add_reference :likes, :post, null: false, foreign_key: true
     rename_column :likes, :user_id, :author_id
+    add_index :likes, :user
+    add_index :likes, :post
   end
 end

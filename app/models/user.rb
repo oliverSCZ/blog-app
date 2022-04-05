e594a132800d465500a6837101e43c3cef7ac0e0
Oliver Coimbra <olivercoimbra@gmail.com>
2022-04-05 09:24:01 -0400

    Add 3 most recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_many :comments
+  has_many :posts
+  has_many :likes
+  def self.three_most_recent_post
+    i=Post.ids.length
+    for a in i-2..i
+    puts Post.find_by(id:i).text
+    i=i-1
+    end
+  end
 end

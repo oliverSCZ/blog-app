07ade3976023d725bfc65942c53f3c1adc757ba3
Oliver Coimbra <olivercoimbra@gmail.com>
2022-04-05 09:24:49 -0400

    Add method updates likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  after_save :update_comment_counter
+
+  def self.update_comment_counter
+    Post.increment_counter(:comments_counter, Post.ids)
+   end
 end

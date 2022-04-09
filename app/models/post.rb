7dac7444b9db2712a3b3bb51b2c1014f5832a7ee
Oliver Coimbra <olivercoimbra@gmail.com>
2022-04-09 13:57:51 -0400

    Add a seed for avoid error in user view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  has_many :comments, foreign_key: :author_id
-  has_many :likes
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: :author_id, dependent: :destroy
   after_save :update_counter
 
   validates :title, presence: true, length: { maximum: 250 }

7d9d1569c5687e5090f0b4b093f36c9440fdbf04
Oliver <olivercoimbra@gmail.com>
2022-04-16 17:08:06 -0400

    Add some test

--- spec/views/post_spec.rb
+++ spec/views/post_spec.rb
@@ -20,8 +20,8 @@ RSpec.describe 'Post', type: :feature do
     end
 
     if @post1.comments.empty?
-      @comment1 = @post1.comments.create(text: 'Comment text 1', author_id: @first_user.id)
-      @comment2 = @post1.comments.create(text: 'Comment text 2', author_id: @first_user.id)
+      @comment1 = Comment.create(text: 'Comment text 1', author_id: @first_user.id)
+      @comment2 = Comment.create(text: 'Comment text 2', author_id: @first_user.id)
     else
       @comment1 = @post1.comments.find(1)
       @comment2 = @post1.comments.find(2)
@@ -38,21 +38,21 @@ RSpec.describe 'Post', type: :feature do
       expect(all_images.count).to eq(1)
     end
 
-  #   it 'See the user\'s username' do
-  #     expect(page).to have_content('Tom')
-  #   end
+    # it 'See the user\'s username' do
+    #   expect(page).to have_content('Tom')
+    # end
 
-  #   it 'See the number of posts each user has written' do
-  #     expect(page).to have_content("Posts: #{@first_user.posts.count}")
-  #   end
+    # it 'See the number of posts each user has written' do
+    #   expect(page).to have_content("Posts: #{@first_user.posts.count}")
+    # end
 
-  #   it 'See the post title' do
-  #     expect(page).to have_content(@post.title)
-  #   end
+    # it 'See the post title' do
+    #   expect(page).to have_content(@post.title)
+    # end
 
-  #   it 'See the post body' do
-  #     expect(page).to have_content(@post.text)
-  #   end
+    # it 'See the post body' do
+    #   expect(page).to have_content(@post.text)
+    # end
 
   #   it 'See the first comments on a post.' do
   #     # expect(page).to have_current_path user_posts_path(@first_user)
